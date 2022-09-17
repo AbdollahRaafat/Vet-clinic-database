72c67d88a1b3258c76e7a26b98de2fc8c43b2e2a
abdollahraafat <abdollah.raafat.daif@gmail.com>
2022-09-17 17:42:16 +0200

    Add the transaction to the queries file

--- queries.sql
+++ queries.sql
@@ -23,3 +23,46 @@ WHERE name !='Gabumon';
 
 SELECT * FROM ANIMALS
 WHERE weitght_kg >= 10.4 AND weitght_kg <= 17.3;
+
+-- the first transation 
+BEGIN;
+vet_clinic=*# UPDATE animals
+vet_clinic-*# SET species= 'unspecified';
+ROLLBACK;
+
+
+-- the second transation
+BEGIN;
+UPDATE animals
+SET species='digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species='pokemon'
+WHERE species IS NULL;
+COMMIT;
+
+
+-- the third transation
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+
+-- the fourth transation
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth>'2022-01-01';
+
+SAVEPOINT sp1;
+
+UPDATE animals
+SET weight_kg=weight_kg*-1;
+
+ROLLBACK TO SAVEPOINT sp1;
+
+UPDATE animals
+SET weight_kg=weight_kg*-1
+WHERE weight_kg<0;
+
+COMMIT;
