a0c53798251c956e272beee4d86f32fadf6887a5
abdollahraafat <abdollah.raafat.daif@gmail.com>
2022-09-17 20:55:34 +0200

    Create join table for visits and specializations

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE DATABASE vet_clinic;
 CREATE TABLE animals(
   id INT,
@@ -14,21 +13,14 @@ CREATE TABLE animals(
   FOREIGN KEY(species_id) REFERENCES species(id),
   ADD FOREIGN KEY(owner_id) REFERENCES species(id)
 );
-
 ALTER TABLE animals 
-    
-ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
-  
 ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
 ALTER TABLE animals
 DROP COLUMN species;
-
 ALTER TABLE animals ADD species_id INT;
 ALTER TABLE animals ADD FOREIGN KEY(species_id) REFERENCES species(id);
-
 ALTER TABLE animals ADD owner_id INT;
 ALTER TABLE animals ADD FOREIGN KEY(owner_id) REFERENCES species(id);
-
 CREATE TABLE owners(
  id INT GENERATED ALWAYS AS IDENTITY,
  full_name TEXT,
@@ -39,4 +31,22 @@ CREATE TABLE species(
  id GENERATED ALWAYS AS IDENTITY,
  name TEXT,
  PRIMARY KEY(id)
+);
+
+CREATE TABLE vets(
+ id INT GENERATED ALWAYS AS IDENTIY,
+ name TEXT,
+ age INT,
+ date_of_graduation DATE
+);
+
+CREATE TABLE specializations(
+ vet TEXT,
+ specie TEXT
+);
+
+CREATE TABLE visits(
+ vet TEXT,
+ animal TEXT,
+ date_visited DATE
 );
