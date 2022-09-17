bf0b8d32329f75534086b6d4d7a3f42d4c2765c9
abdollahraafat <abdollah.raafat.daif@gmail.com>
2022-09-17 20:41:57 +0200

    Update species_id column on animals table

--- queries.sql
+++ queries.sql
@@ -1,102 +1,67 @@
-/*Queries that provide answers to the questions from all projects.*/
-
-SELECT * FROM ANIMALS
-WHERE name LIKE '%mon';
-
-SELECT name FROM ANIMALS
-WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
-
-SELECT name FROM ANIMALS
-WHERE neutered = TRUE AND escape_attempts< 3;
-
-SELECT date_of_birth FROM ANIMALS
-WHERE name IN ('Agumon','Pikachu');
-
-SELECT escape_attempts, weitght_kg FROM ANIMALS
-WHERE weitght_kg> 10.5;
-
-SELECT * FROM ANIMALS
-WHERE neutered IS TRUE;
-
-SELECT * FROM ANIMALS
-WHERE name !='Gabumon';
-
-SELECT * FROM ANIMALS
-WHERE weitght_kg >= 10.4 AND weitght_kg <= 17.3;
-
--- the first transation 
+-- QUERIES (1)
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = TRUE;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+-- TRANSACTONS
 BEGIN;
-vet_clinic=*# UPDATE animals
-vet_clinic-*# SET species= 'unspecified';
+UPDATE animals
+SET species = 'unspecified';
 ROLLBACK;
-
-
--- the second transation
+-----
 BEGIN;
 UPDATE animals
-SET species='digimon'
+SET species = 'digimon'
 WHERE name LIKE '%mon';
-
 UPDATE animals
-SET species='pokemon'
+SET species = 'pokemon'
 WHERE species IS NULL;
 COMMIT;
-
-
--- the third transation
+-----
+-- Hold Breath
 BEGIN;
 DELETE FROM animals;
 ROLLBACK;
-
-
--- the fourth transation
+-- Continue Breathing
+-----
 BEGIN;
 DELETE FROM animals
-WHERE date_of_birth>'2022-01-01';
-
-SAVEPOINT sp1;
-
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals 
+SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
 UPDATE animals
-SET weight_kg=weight_kg*-1;
-
-ROLLBACK TO SAVEPOINT sp1;
-
-UPDATE animals
-SET weight_kg=weight_kg*-1
-WHERE weight_kg<0;
-
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
 COMMIT;
-
-
-
+-----
+-- QUERIES (2)
 -- How many animals are there?
 SELECT COUNT(*) FROM animals;
-
-
---How many animals have never tried to escape?
-SELECT escape_attempts, COUNT(*)
-FROM animals
-GROUP BY escape_attempts
-HAVING escape_attempts=0;
-
---What is the average weight of animals?
-SELECT AVG(weight_kg)
-FROM animals;
-
---Who escapes the most, neutered or not neutered animals?
-SELECT neutered,SUM(escape_attempts)
-FROM animals
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals 
+WHERE escape_attempts = 0;
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, MAX(escape_attempts) FROM animals 
 GROUP BY neutered;
-
-
---What is the minimum and maximum weight of each type of animal?
-SELECT species, MIN(weight_kg),MAX(weight_kg)
-FROM animals
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals
+GROUP BY species;
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
 GROUP BY species;
 
-
---What is the average number of escape attempts per animal type of those born between 1990 and 2000?
-SELECT species ,AVG(escape_attempts)
-FROM animals
-WHERE date_of_birth >='1990-01-01' AND date_of_birth <='2000-01-01'
-GROUP BY species;
+-- QUERIES (3)
+-- Modify your inserted animals so it includes the species_id value
+UPDATE animals SET species_id = 2
+WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1
+WHERE species_id IS NULL;
