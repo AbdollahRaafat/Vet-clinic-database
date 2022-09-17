98ef1a9041c74ac6cdcd43a127d431b057fdb80b
abdollahraafat <abdollah.raafat.daif@gmail.com>
2022-09-17 20:40:35 +0200

    Add foreign keys to animal table

--- schema.sql
+++ schema.sql
@@ -1,14 +1,40 @@
-/* Database schema to keep the structure of entire database. */
-
 CREATE DATABASE vet_clinic;
-
 CREATE TABLE animals(
-id INT PRIMARY KEY,
-name VARCHAR,
-date_of_birth DATE,
-escape_attempts INT,
-neutered BOOLEAN,
-weitght_kg DECIMAL);
+  id INT,
+  name TEXT,
+  date_of_birth DATE, 
+  escape_attemps INT,
+  neutered BOOLEAN,    
+  weight_kg DEC,
+  species_id INT,
+  owner_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(species_id) REFERENCES species(id),
+  ADD FOREIGN KEY(owner_id) REFERENCES species(id)
+);
 
+ALTER TABLE animals 
+    
+ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
+  
+ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
 ALTER TABLE animals
-ADD species TEXT ;
+DROP COLUMN species;
+
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY(species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY(owner_id) REFERENCES species(id);
+
+CREATE TABLE owners(
+ id INT GENERATED ALWAYS AS IDENTITY,
+ full_name TEXT,
+ age INT,
+ PRIMARY KEY(id)
+);
+CREATE TABLE species(
+ id GENERATED ALWAYS AS IDENTITY,
+ name TEXT,
+ PRIMARY KEY(id)
+);
