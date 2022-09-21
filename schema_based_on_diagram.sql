deeedd50c88b0417f719146af644945989ae1ce2
Virag Kormoczy <vkormoczy@gmail.com>
2022-09-21 16:27:51 +0300

    Add invoices and invoices_items table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -16,3 +16,7 @@ id INT PRIMARY KEY,
 type varchar,
 name varchar,
 foreign key(id) references  medical_histories(id));
+
+create table invoices( id INT PRIMARY KEY, total_amount DEC, generated_at date, payed_at date, medical_history_id INT, FOREIGN KEY (medical_history_id) REFERENCES MEDICAL_HISTORIES(ID)); 
+
+create table invoice_items( id INT, unit_price DEC, quantity INT, total_price DEC, invoice_id INT, treatment_id int, FOREIGN KEY (invoice_id) REFERENCES invoices(ID), FOREIGN KEY (treatment_id) REFERENCES treatments(ID)); 
